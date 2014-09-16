fc3e836ec358ee3a71b253255e622e4d930518a3
swarlesbarkely <zmday22@gmail.com>
2014-09-15 19:08:22 -0500

    Update cronsetup.sh

--- Z_cron/cronsetup.sh
+++ Z_cron/cronsetup.sh
@@ -5,21 +5,21 @@ mount -o rw,remount /system;
 ### Make proper directives ###
 
 mkdir /etc/cron.d/;
-mkdir /etc/cron.d/crontabs/;
-mkdir /etc/cron.d/crontabs/minutely/;
-mkdir /etc/cron.d/crontabs/daily/;
-mkdir /etc/cron.d/crontabs/hourly/;
 
 ### Move files ###
 
 cp /sdcard/Z_cron/cron /etc/init.d;
-cp /sdcard/Z_cron/root /etc/cron.d/crontabs;
-cp /sdcard/Z_cron/minutely/* /etc/cron.d/minutely;
-cp /sdcard/Z_cron/daily/* /etc/cron.d/daily;
-
+cp -R /sdcard/Z_cron/hourly /etc/cron.d/hourly;
+cp -R /sdcard/Z_cron/daily /etc/cron.d/daily;
+cp -R /sdcard/Z_cron/minutely /etc/cron.d/minutely;
+cp -R /sdcard/Z_cron/crontabs /etc/cron.d/crontabs;
 
 chmod 755 /etc/init.d/cron;
 
+chmod 755 /etc/cron.d;
+
 chmod 755 /etc/cron.d/*;
+
+chmod 755 /etc/cron.d/*/*;
 
 mount -o ro,remount /system;
