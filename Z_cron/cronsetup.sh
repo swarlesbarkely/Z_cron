7988bc4d82dfaadcb0480f0d0cb56d2d3bca3993
swarlesbarkely <zmday22@gmail.com>
2014-09-26 23:43:44 -0500

    Update cronsetup.sh

--- Z_cron/cronsetup.sh
+++ Z_cron/cronsetup.sh
@@ -11,10 +11,10 @@ mkdir /etc/cron.d/;
 ### Move files ###
 
 cp $dir/Z_cron/cron /etc/init.d;
-cp -R $dir/Z_cron/hourly /etc/cron.d/hourly;
-cp -R $dir/Z_cron/daily /etc/cron.d/daily;
-cp -R $dir/Z_cron/minutely /etc/cron.d/minutely;
-cp -R $dir/Z_cron/crontabs /etc/cron.d/crontabs;
+cp -R $dir/hourly /etc/cron.d/hourly;
+cp -R $dir/daily /etc/cron.d/daily;
+cp -R $dir/minutely /etc/cron.d/minutely;
+cp -R $dir/crontabs /etc/cron.d/crontabs;
 
 chmod 755 /etc/init.d/cron;
 
